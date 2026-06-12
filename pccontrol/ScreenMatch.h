d2f660d8fb29690874439274de7ab16f712c99b8
Epic0001 <110989057+Epic0001@users.noreply.github.com>
2026-06-12 09:41:48 -0400

    Implement image_match using Accelerate NCC — no OpenCV required

--- pccontrol/ScreenMatch.h
+++ pccontrol/ScreenMatch.h
@@ -1,10 +1,13 @@
 #ifndef SCREEN_MATCH_H
 #define SCREEN_MATCH_H
 
-#endif
+#import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+
 CGRect screenMatchFromRawData(UInt8 *eventData, NSError **error);
 
 @interface ScreenMatch : NSObject
 + (CGRect)matchCurrentScreenWithTemplate:(NSString*)templatePath maxTryTimes:(int)mtt acceptableValue:(float)av scaleRation:(float)sr error:(NSError**)err;
-
 @end
+
+#endif
